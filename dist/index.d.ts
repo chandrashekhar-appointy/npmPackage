eebe26c87e8be9757c8e11b31360483dbc4327c1
chandrashekhar-appointy <chandrashekhar@appointy.com>
2025-07-16 16:54:50 +0530

    fix: build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,9 +2,11 @@
  * Multiplies a number by 2
  * @param input - The number to multiply by 2
  * @returns The result of multiplying input by 2
+ * @throws {Error} If input is not a finite number
  */
 export declare function multiplyByTwo(input: number): number;
 /**
  * Default export for convenience
  */
 export default multiplyByTwo;
+//# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.multiplyByTwo = void 0;
+exports.multiplyByTwo = multiplyByTwo;
 /**
  * Multiplies a number by 2
  * @param input - The number to multiply by 2
  * @returns The result of multiplying input by 2
+ * @throws {Error} If input is not a finite number
  */
 function multiplyByTwo(input) {
+    if (!Number.isFinite(input)) {
+        throw new Error('Input must be a finite number');
+    }
     return input * 2;
 }
-exports.multiplyByTwo = multiplyByTwo;
 /**
  * Default export for convenience
  */
 exports.default = multiplyByTwo;
+//# sourceMappingURL=index.js.map
